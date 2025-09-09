56b03f6185483d84a83635b75312b170d003a656
Technical Maintenance Logistics <logistics@compuclinic.co.za>
2025-09-09 13:14:05 +0000

    yes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -94,6 +94,13 @@ export default function DashboardLayout({
             <span className="material-symbols-outlined">campaign</span>
             <p className="text-sm font-medium leading-normal">Alerts</p>
           </Link>
+           <Link
+            className="flex items-center gap-3 px-3 py-2 text-gray-400 hover:bg-[#233648] hover:text-white rounded-md"
+            href="/dashboard/whats-new"
+          >
+            <span className="material-symbols-outlined">new_releases</span>
+            <p className="text-sm font-medium leading-normal">What's New</p>
+          </Link>
           <Link
             className="flex items-center gap-3 px-3 py-2 text-gray-400 hover:bg-[#233648] hover:text-white rounded-md"
             href="/dashboard/permissions"
